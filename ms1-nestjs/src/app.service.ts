d0f427d4bfa4f4635e2c31979c50b3ec2d3f05e2
houra-labellevie <houra@labellevie.jp>
2025-04-10 19:38:03 +0900

    git provider ミスってたので修正

--- ms1-nestjs/src/app.service.ts
+++ ms1-nestjs/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Im micro service 2!';
+    return 'Hello World! Im micro service 1!';
   }
 }
